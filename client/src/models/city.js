46162d801b843c9c1cf7740b6d6274742a49c70a
Old Prego <spymasterluke@gmail.com>
2021-07-19 21:09:11 -0500

    I forgot to save a file, like an idiot

--- client/src/models/city.js
+++ client/src/models/city.js
@@ -2,16 +2,22 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const stateSchema = new Schema({
+const citySchema = new Schema({
     id: {
         type: Number
     },
     name: {
         type: String,
+        required: true
     },
-    
+    state: {
+        type: String,
+        required: true
+    },
+    restaurants: Array,
+    landmarks: Array,
 });
 
-const State = mongoose.model("State", stateSchema);
+const City = mongoose.model("City", citySchema);
 
-module.exports = State;
+module.exports = City;
