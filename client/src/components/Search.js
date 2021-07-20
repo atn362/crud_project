ecc1572d920faa501dd701b07c21584d6fdb0029
Alex Norton <atn362@gmail.com>
2021-07-20 15:23:05 -0500

    search page updated with text field and button

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from 'react';
+import { Textfield, Button } from 'react-mdl';
 
 function Search() {
 
     return(
         <div>
         <h1>Search Design Options</h1>
-
+        <Textfield
+    onChange={() => {}}
+    label="Text..."
+    style={{width: '200px'}}
+/>
+        <Button>Submit</Button>
         </div>
     );
 };
