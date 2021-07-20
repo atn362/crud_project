628c3dbc3588b54bdc3ba2327ff5ea794c2d97b1
Old Prego <spymasterluke@gmail.com>
2021-07-19 19:52:06 -0500

    Added comment model

--- client/src/models/user.js
+++ client/src/models/user.js
@@ -11,8 +11,12 @@ const userSchema = new Schema({
     username: {
         type: String,
         required: true,
-    },
-
+        unique: true,
+        trim: true,
+        minlength: 6
+    },  
+},{
+    timestamps: true,
 });
 
 const User = mongoose.model("User", userSchema);
