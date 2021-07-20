e775ee10b1aeef135fd855c1e51f3567169e5486
Alex Norton <atn362@gmail.com>
2021-07-20 17:41:31 -0500

    updates to the search and home pages

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,19 +1,23 @@
-import React from 'react';
-import { Textfield, Button } from 'react-mdl';
+import React from "react";
+import { Textfield, Button } from "react-mdl";
+import Footer from "./Footer";
 
 function Search() {
-
-    return(
-        <div>
-        <h1>Search Design Options</h1>
-        <Textfield
-    onChange={() => {}}
-    label="Text..."
-    style={{width: '200px'}}
-/>
-        <Button>Submit</Button>
-        </div>
-    );
-};
+  return (
+    <div>
+      <h1>Search Design Options</h1>
+      <Textfield
+        onChange={() => {}}
+        label="Text..."
+        style={{ width: "200px" }}
+      />
+      <Button>
+        Submit
+      </Button>
+      <hr />
+      <Footer />
+    </div>
+  );
+}
 
 export default Search;
