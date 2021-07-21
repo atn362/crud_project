484258581c93b7f11a32b4c48e2bd1d5e1b5f275
Alex Norton <atn362@gmail.com>
2021-07-20 20:19:44 -0500

    home page updated with cards

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,11 +1,43 @@
 import React from 'react'
+import { Card, CardTitle, CardActions, CardText, CardMenu } from 'react-mdl';
 
 function Home() {
 
     return(
         <div>
-        <h1>Home Design Options</h1>
-
+            <h1>Travel Blog Homepage</h1>
+      <Card shadow={0} style={{width: '512px', margin: 'auto', boxShadow: "5px 10px 15px black"}}>
+    <CardTitle style={{color: '#fff', height: '176px', background: 'url(http://www.getmdl.io/assets/demos/welcome_card.jpg) center / cover'}}>Welcome</CardTitle>
+    <CardText>
+        Travel Blog with all necessary information
+    </CardText>
+    <CardActions border>
+    </CardActions>
+    <CardMenu style={{color: '#fff'}}>
+    </CardMenu>
+</Card>
+<hr/>
+<Card shadow={0} style={{width: '512px', margin: 'auto', boxShadow: "5px 10px 15px black"}}>
+    <CardTitle style={{color: '#fff', height: '176px', background: 'url(http://www.getmdl.io/assets/demos/welcome_card.jpg) center / cover'}}>Welcome</CardTitle>
+    <CardText>
+        Travel Blog with all necessary information
+    </CardText>
+    <CardActions border>
+    </CardActions>
+    <CardMenu style={{color: '#fff'}}>
+    </CardMenu>
+</Card>
+<hr/>
+<Card shadow={0} style={{width: '512px', margin: 'auto', boxShadow: "5px 10px 15px black"}}>
+    <CardTitle style={{color: '#fff', height: '176px', background: 'url(http://www.getmdl.io/assets/demos/welcome_card.jpg) center / cover'}}>Welcome</CardTitle>
+    <CardText>
+        Travel Blog with all necessary information
+    </CardText>
+    <CardActions border>
+    </CardActions>
+    <CardMenu style={{color: '#fff'}}>
+    </CardMenu>
+</Card>
         </div>
     );
 };
