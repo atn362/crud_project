8729755a480251a58676f78da1e56a92808a5949
Alex Norton <atn362@gmail.com>
2021-07-20 21:01:55 -0500

    login and search pages updated

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -7,13 +7,13 @@ function Search() {
     <div>
       <h1>Search Design Options</h1>
       <container style={{display: 'flex', justifyContent: 'center'}} className="search-box">
-      <Card> 
+      <Card style={{background:"#d3d3d3", display: 'flex', justifyContent: 'center', alignItems: 'center', borderRadius: '25%'}}> 
       <Textfield
         onChange={() => {}}
-        label="Text..."
-        style={{ width: "200px" }}
+        label="Enter Desired City"
+        style={{ width: "200px", color: 'black' }}
       />
-      <Button style={{background: 'blue', width: '50%'}}>
+      <Button style={{background: 'blue', width: '50%', color: 'grey', borderRadius: '25%'}}>
         Submit
       </Button>
       </Card>
