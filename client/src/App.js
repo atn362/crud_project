9dde813cf14bb56b6ed5498df170c728d23a50ad
DeerRan <77216956+DeerRan@users.noreply.github.com>
2021-07-21 20:22:35 -0500

    Changed the layout and messing with background-images

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import { Layout, Header, Drawer, Navigation, Content } from "react-mdl";
+import { Layout, Header, Navigation, Content } from "react-mdl";
 import { Link } from "react-router-dom";
 import Main from "./components/Main";
 
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <Layout>
-        <Header  transparent title="Crime Stats Blog" style={{ color: "white" }}>
+        <Header title="Crime Stats Travel Blog" style={{ color: "white", backgroundColor:"rgb(0, 0, 0, .8)"}}>
           <Navigation>
             <Link to="/">Home</Link>
             <Link to="/login">Login</Link>
@@ -16,14 +16,6 @@ function App() {
             <Link to="/comment">Comment</Link>
           </Navigation>
         </Header>
-        <Drawer title="Crime Stats Blog">
-          <Navigation>
-            <Link to="/">Home</Link>
-            <Link to="/login">Login</Link>
-            <Link to="/search">Search</Link>
-            <Link to="/comment">Comment</Link>
-          </Navigation>
-        </Drawer>
         <Content>
           <Main></Main>
         </Content>
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,11 +1,40 @@
 import React from 'react'
+import { Grid, Cell } from 'react-mdl'
 
 function Home() {
 
     return(
-        <div>
-        <h1>Home Design Options</h1>
+        <div className='homebody'>
+            <div className='section-1'>
+                <div className='titlediv'>
+                    <h1 className='title'>Home Design Options</h1>
+                </div>
 
+                <div className='description'>
+                    <Grid className='grid'>
+                        <Cell col={12}>
+                            <h2> Hello! </h2>
+                        </Cell>
+
+                        <Cell col={12} className='Pcell'>
+                            <p>This is a travel blog that gives crime-related information on the destinations that you want to travel to!</p>
+                        </Cell>
+
+                        <Cell col={12} className='Pcell'>
+                            <p>This project was made by a small team of five coders practing React so if you have feedback or questions, shoot us an email! </p>  
+                        </Cell>
+                    </Grid>
+                </div>
+
+                <div className='scrollPrompt'>
+                    <h2>Scroll Down</h2>
+                    <h2>V</h2>
+                </div>
+            </div>
+
+            <div className='section-2'>
+
+            </div>
         </div>
     );
 };
