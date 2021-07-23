ea7adf0d498c3ef20eac9262273e36267205d860
Alex Norton <atn362@gmail.com>
2021-07-23 16:02:30 -0500

    slight changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import { Layout, Header, Navigation, Content } from "react-mdl";
+import { Layout, Header, Navigation, Content, Drawer } from "react-mdl";
 import { Link } from "react-router-dom";
 import Main from "./components/Main";
 
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <Layout>
-        <Header title="Crime Stats Travel Blog" style={{ color: "white", backgroundColor:"rgb(0, 0, 0, .8)"}}>
+        <Header title="Travel Blog" style={{ color: "white", backgroundColor:"rgb(0, 0, 0, .8)"}}>
           <Navigation>
             <Link style={{color: 'white', fontWeight: '500'}} to="/">Home</Link>
             <Link to="/login">Login</Link>
@@ -17,6 +17,16 @@ function App() {
             <Link to="/comment">Comment</Link>
           </Navigation>
         </Header>
+        <div className="drawer">
+        <Drawer title="Title">
+            <Navigation>
+            <Link style={{color: 'white', fontWeight: '500'}} to="/">Home</Link>
+            <Link to="/login">Login</Link>
+            <Link to="/search">Search</Link>
+            <Link to="/comment">Comment</Link>
+            </Navigation>
+        </Drawer>
+        </div>
         <Content>
           <Main></Main>
         </Content>
