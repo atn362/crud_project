ecf69c8d457a3515b2c8d5e02fe2c3fd914c780f
Jason <jrosasrjf@gmail.com>
2021-07-27 19:15:37 -0500

    added in WebTokens.js file for login authentication

---
+++ client/src/webTokens.js
@@ -0,0 +1,15 @@
+/* -- Basic Imports --  */
+const jwt = require('jsonwebtoken');
+
+const secret = 'mysecretssshhhhhhh';
+const expiration = '2h';
+
+
+
+/* -- Functions -- */
+module.exports = {
+    signToken: function ({ email, username, _id }) {
+      const payload = { email, username, _id };
+      return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+    },
+}  
