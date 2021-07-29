5275e4547995761d23880d617468618d06465df2
Old Prego <spymasterluke@gmail.com>
2021-07-28 19:05:16 -0500

    Added results and got them to show up

--- client/src/components/Results.js
+++ client/src/components/Results.js
@@ -1,47 +1,34 @@
 import React from "react";
-import AllLocations from './Locations';
+import {Restaurants} from './restaurants';
+import {Lodging} from './lodging';
+import {Entertainment} from './entertainment';
 
-function Results()
-{
-    return AllLocations.map((location, index) => {
-        return(
-            <div className="resultscolumn" key={index}>
-                <h2>{location.name}</h2>
-                <p>{location.type}</p>
-                <p>Rating: {location.rating}</p>
-                <img href={location.image} alt="locationImage"/>
+function Results() {
+    return (
+        //For background image, have searched city and state as background image search paramaters!!!
+        <div className='resultsbody' style={{backgroundImage: `url("https://source.unsplash.com/featured/?Minnesota,Minneapolis")`}}>
+            <div className='resultstitlediv'>
+                <h1 className='resultstitle'>Minneapolis, MN</h1>
             </div>
-        )
-    })
+
+            <div  className="resultsdiv ">
+                <div className="restaurantscolumn">
+                    <h2> Restaurants </h2>
+                    <Restaurants />
+                </div>
+
+                <div className="lodgingcolumn">
+                    <h2> Hotels </h2>
+                    <Lodging />
+                </div>
+
+                <div className="entertainmentcolumn">
+                    <h2> Entertainment </h2>
+                    <Entertainment />
+                </div>
+            </div >
+        </div>
+    );
 }
-
-// function Results() {
-//     return (
-//         //For background image, have searched city and state as background image search paramaters!!!
-//         <div className='resultsbody' style={{backgroundImage: `url("https://source.unsplash.com/featured/?Minnesota,Minneapolis")`}}>
-//             <div className='resultstitlediv'>
-//                 <h1 className='resultstitle'>Minneapolis, MN</h1>
-//             </div>
-
-//             <div  className="resultsdiv ">
-//                 <div className="resultscolumn">
-//                     <h2> Restaurants </h2>
-//                     <p></p>
-//                 </div>
-
-//                 <div className="resultscolumn">
-//                     <h2> Hotels </h2>
-
-//                     <p></p>
-//                 </div>
-
-//                 <div className="resultscolumn">
-//                     <h2> Landmarks </h2>
-//                     <p></p>
-//                 </div>
-//             </div >
-//         </div>
-//     );
-// }
 
 export default Results;
